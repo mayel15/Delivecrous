475ab9bb5db4703fe546f30ce2a3ea9f1ee820a8
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-01-22 19:45:38 +0100

    link between back and front

---
+++ cart.js
@@ -0,0 +1,30 @@
+// 
+let dishes = []
+const fetchDishes = async () => {
+    await fetch('http://localhost:3000/cart/', { method: 'GET' }).then((response) => response.json())
+        .then((promise) => {
+            dishes = promise
+            console.log(promise)
+        })
+
+};
+const fetchDishesDisplay = async () => {
+    await fetchDishes();
+    let affichage;
+    for (let i = 0; i < dishes.length; i++) {
+        affichage += `<div class="card col-lg-4 card-dish" style="width: 18rem;">
+                <img class="card-img-top" src="images/yassa.jpeg" alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">${dishes[i].name}<span class="price">${dishes[i].price.toString()}€</span></h5>
+                  <p class="card-text">${dishes[i].description}</p>
+                  <a href="#" class="btn btn-primary">voir</a>
+                </div>
+            </div>
+            `
+    }
+    console.log(affichage)
+    document.querySelector('.all-dishes').innerHTML = affichage
+}
+
+fetchDishesDisplay();
+
--- index.js
+++ index.js
@@ -10,16 +10,19 @@ const fetchDishes = async () => {
 };
 const fetchDishesDisplay = async () => {
     await fetchDishes();
-    let affichage;
+    let affichage="";
     for (let i = 0; i < dishes.length; i++) {
         affichage += `<div class="card col-lg-4 card-dish" style="width: 18rem;">
-                <img class="card-img-top" src="images/yassa.jpeg" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title">${dishes[i].name}<span class="price">${dishes[i].price.toString()}€</span></h5>
-                  <p class="card-text">${dishes[i].description}</p>
-                  <a href="#" class="btn btn-primary">voir</a>
-                </div>
-            </div>
+        <img class="card-img-top" src="images/yassa.jpeg" alt="dish-image">
+        <div class="card-body">
+          <h5 class="card-title">${dishes[i].name}<span class="price">${dishes[i].price}€</span></h5>
+          <p class="card-text">${dishes[i].description}</p>
+          <form action="http://localhost:3000/cart/${dishes[i]._id}" method="post">
+          <a href="http://localhost:3000/dishes/${dishes[i]._id}" class="btn btn-primary">Voir</a>
+          <button type="submit" name="submit" class="btn btn-primary">Ajouter au panier</button>
+          </form>
+          </div>
+    </div>
             `
     }
     console.log(affichage)
