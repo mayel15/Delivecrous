ca3e49cb96aaba735cc13effc1434b815bb85139
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-01-22 17:27:25 +0100

    some modif

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // 
 let dishes = []
-    const fetchDishes = async () => {
-        await fetch('http://localhost:3000/dishes', {method: 'GET'}).then((response)=>response.json())
-            .then((promise)=>{
-                dishes = promise
-                console.log(promise)
-            })
-        
-    };
-    const fetchDishesDisplay = async () => {
-        await fetchDishes();
-        let affichage;
-        for(let i=0; i< dishes.length ; i++){
-            affichage += `<div class="card col-lg-4 card-dish" style="width: 18rem;">
+const fetchDishes = async () => {
+    await fetch('http://localhost:3000/dishes', { method: 'GET' }).then((response) => response.json())
+        .then((promise) => {
+            dishes = promise
+            console.log(promise)
+        })
+
+};
+const fetchDishesDisplay = async () => {
+    await fetchDishes();
+    let affichage;
+    for (let i = 0; i < dishes.length; i++) {
+        affichage += `<div class="card col-lg-4 card-dish" style="width: 18rem;">
                 <img class="card-img-top" src="images/yassa.jpeg" alt="Card image cap">
                 <div class="card-body">
                   <h5 class="card-title">${dishes[i].name}<span class="price">${dishes[i].price.toString()}€</span></h5>
@@ -21,10 +21,10 @@ let dishes = []
                 </div>
             </div>
             `
-        }
-        console.log(affichage)
-        document.querySelector('.all-dishes').innerHTML = affichage
     }
-    
+    console.log(affichage)
+    document.querySelector('.all-dishes').innerHTML = affichage
+}
+
 fetchDishesDisplay();
 
